c3ef8d51a1cec56539fe90ddc3a5752a1c3f57ee
jkea1 <99384699+jkea1@users.noreply.github.com>
2023-10-25 17:49:22 +0900

    기본 페이지 틀 생성했습니다.  (#12)
    * feat: 미션 관련 페이지 및 레이아웃 생성
    
    * style: 스타일 초기화

---
+++ one-day-hero/src/app/mission/detail/[slug]/page.tsx
@@ -0,0 +1,5 @@
+const MissionDetailPage = () => {
+  return <div>MissionDetailPage</div>;
+};
+
+export default MissionDetailPage;
---
+++ one-day-hero/src/app/mission/list/ongoing/page.tsx
@@ -0,0 +1,5 @@
+const OngoingMissionPage = () => {
+  return <div>OngoingMissionPage</div>;
+};
+
+export default OngoingMissionPage;
