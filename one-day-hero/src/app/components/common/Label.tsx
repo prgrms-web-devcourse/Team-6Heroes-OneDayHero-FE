6df3067fc842ff69664018833e656be1a419e599
jkea1 <jinkyng7890@gmail.com>
2023-10-26 16:17:51 +0900

    feat: 라벨 컴포넌트 생성 했습니다.
    색깔 속성은 팔레트 완성 되면 수정하겠습니다.

--- one-day-hero/src/app/components/common/Label.tsx
+++ one-day-hero/src/app/components/common/Label.tsx
@@ -2,25 +2,22 @@ interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
   size?: "sm" | "md" | "lg";
 }
 
-const Label = ({ size = "md", children, className, ...props }: LabelProps) => {
-  const textSizes = {
-    sm: "text-xs",
-    md: "text-sm",
-    lg: "text-lg"
+const Label = ({
+  size = "md",
+  children,
+  className = "",
+  ...props
+}: LabelProps) => {
+  const sizes = {
+    sm: "text-xs w-16 h-4 py-[0.05rem] rounded-3xl",
+    md: "text-sm w-28 h-6 py-[0.2rem] px-1 rounded-3xl",
+    lg: "text-lg min-w-[4.5rem] h-8 py-0.5 px-3 rounded-xl border border-1 border-zinc-300"
   };
 
-  const widthSizes = {
-    sm: "w-16",
-    md: "w-28",
-    lg: "w-20"
-  };
-
-  const defaultStyle = "rounded-3xl p-2 bg-red-200";
+  const defaultStyle = "my-auto bg-blue-200 text-center";
 
   return (
-    <label
-      className={`${defaultStyle} ${textSizes[size]} ${widthSizes[size]} ${className}`}
-      {...props}>
+    <label className={`${defaultStyle} ${sizes[size]} ${className}`} {...props}>
       {children}
     </label>
   );
