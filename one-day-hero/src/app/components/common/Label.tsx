6bef5e886ea087901b0021fe60ca11ce2c19cd71
jkea1 <jinkyng7890@gmail.com>
2023-10-26 17:14:41 +0900

    style: y축 중앙 정렬 방법 변경

--- one-day-hero/src/app/components/common/Label.tsx
+++ one-day-hero/src/app/components/common/Label.tsx
@@ -9,12 +9,13 @@ const Label = ({
   ...props
 }: LabelProps) => {
   const sizes = {
-    sm: "text-xs min-w-16 h-4 py-[0.05rem] px-4 rounded-3xl",
-    md: "text-sm min-w-28 h-6 py-[0.1rem] px-3 rounded-3xl",
-    lg: "text-lg min-w-[4.5rem] h-8 py-0.5 px-4 rounded-[0.625rem] border border-1 border-zinc-300"
+    sm: "text-xs min-w-16 h-4 px-4 rounded-3xl",
+    md: "text-sm min-w-28 h-6 px-3 rounded-3xl",
+    lg: "text-lg min-w-[4.5rem] h-8 px-4 rounded-[0.625rem] border border-1 border-zinc-300"
   };
 
-  const defaultStyle = "bg-blue-200 text-center";
+  const defaultStyle =
+    "bg-blue-200 text-center inline-flex justify-center items-center";
 
   return (
     <label className={`${defaultStyle} ${sizes[size]} ${className}`} {...props}>
