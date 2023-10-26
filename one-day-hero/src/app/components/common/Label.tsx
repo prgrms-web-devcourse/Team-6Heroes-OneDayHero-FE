740b70463d63a1e97581cdf8344f6326d8e100e6
jkea1 <jinkyng7890@gmail.com>
2023-10-26 16:36:05 +0900

    style: 라벨 컴포넌트 수정

--- one-day-hero/src/app/components/common/Label.tsx
+++ one-day-hero/src/app/components/common/Label.tsx
@@ -9,12 +9,12 @@ const Label = ({
   ...props
 }: LabelProps) => {
   const sizes = {
-    sm: "text-xs w-16 h-4 py-[0.05rem] rounded-3xl",
-    md: "text-sm w-28 h-6 py-[0.2rem] px-1 rounded-3xl",
+    sm: "text-xs min-w-16 h-4 py-[0.05rem] px-4 rounded-3xl",
+    md: "text-sm min-w-28 h-6 py-[0.1rem] px-3 rounded-3xl",
     lg: "text-lg min-w-[4.5rem] h-8 py-0.5 px-3 rounded-xl border border-1 border-zinc-300"
   };
 
-  const defaultStyle = "my-auto bg-blue-200 text-center";
+  const defaultStyle = "bg-blue-200 text-center";
 
   return (
     <label className={`${defaultStyle} ${sizes[size]} ${className}`} {...props}>
