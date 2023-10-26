39e4bf049842aac06801340170c5f7e1f76c9f6f
jkea1 <jinkyng7890@gmail.com>
2023-10-26 16:43:44 +0900

    style: 라벨 스타일 수정

--- one-day-hero/src/app/components/common/Label.tsx
+++ one-day-hero/src/app/components/common/Label.tsx
@@ -11,7 +11,7 @@ const Label = ({
   const sizes = {
     sm: "text-xs min-w-16 h-4 py-[0.05rem] px-4 rounded-3xl",
     md: "text-sm min-w-28 h-6 py-[0.1rem] px-3 rounded-3xl",
-    lg: "text-lg min-w-[4.5rem] h-8 py-0.5 px-3 rounded-xl border border-1 border-zinc-300"
+    lg: "text-lg min-w-[4.5rem] h-8 py-0.5 px-4 rounded-[0.625rem] border border-1 border-zinc-300"
   };
 
   const defaultStyle = "bg-blue-200 text-center";
--- one-day-hero/src/app/page.tsx
+++ one-day-hero/src/app/page.tsx
@@ -1,12 +1,7 @@
-import Label from "./components/common/Label";
-
 const HomePage = () => {
   return (
     <>
       <div>HomePage</div>
-      <Label size="sm">서빙</Label>
-      <Label size="md">서빙</Label>
-      <Label size="lg">서빙</Label>
     </>
   );
 };
