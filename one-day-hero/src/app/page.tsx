13897d7e98d1c24141c4526a003d181b6f544ac9
Jeong-Taeho <117630589+Jeong-Taeho@users.noreply.github.com>
2023-10-27 23:40:17 +0900

    26 미션 목록 아이템 (#32)
    * feat: 미션 목록 아이템 컴포넌트 구현
    
    * feat: 페이지 크기에 넣어보기
    
    * feat: 예시 기본 이미지
    
    * chore: public 경로 추가
    
    * fix: tailwind 클래스이름 위치 변경
    
    * fix: page 충돌 해결
    
    ---------
    
    Co-authored-by: Jeong-Taeho <nai218218@gmail.com>

--- one-day-hero/src/app/page.tsx
+++ one-day-hero/src/app/page.tsx
@@ -1,4 +1,6 @@
 import BottomNav from "@/components/common/BottomNav";
+import MissionListItem from "@/components/common/MissionListItem";
+
 import Button from "@/components/common/Button";
 import DayList from "@/components/common/Day/DayList";
 import Label from "@/components/common/Label";
@@ -7,6 +9,9 @@ const HomePage = () => {
   return (
     <>
       <div>HomePage</div>
+      <div className="w-96 p-5">
+        <MissionListItem />
+      </div>
       <DayList />
       <Button />
       <Label size="lg" />
