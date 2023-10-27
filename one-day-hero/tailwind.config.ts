681c54c019f960610a21d445e2b376a1b4027a42
Howon Shin <shinhw371@gmail.com>
2023-10-27 16:56:11 +0900

    style: 테마 컬러 추가

--- one-day-hero/tailwind.config.ts
+++ one-day-hero/tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        primary: {
+          DEFAULT: "#C5DD7C",
+          lighten: "#D6F088",
+          darken: "#B0C474"
+        },
+        sub: {
+          DEFAULT: "#FF8551",
+          lighten: "#FFAA7A",
+          darken: "#D77247"
+        },
+        active: {
+          DEFAULT: "#EB6A4E",
+          lighten: "#FFA08B",
+          darken: "#CC634C"
+        },
+        cancel: {
+          DEFAULT: "#ABABAB",
+          lighten: "#D6D6D6",
+          darken: "#717171"
+        },
+        inactive: {
+          DEFAULT: "#D8D8D8",
+          lighten: "#EFEFEF",
+          darken: "#939393"
+        }
+      }
+    }
   },
-  plugins: [],
-}
-export default config
+  plugins: []
+};
+export default config;
