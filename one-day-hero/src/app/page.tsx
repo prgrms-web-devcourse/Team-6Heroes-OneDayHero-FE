1859585e6cadc03c7ec221515b88f40b59930434
Jeong-Taeho <nai218218@gmail.com>
2023-10-27 14:55:15 +0900

    feat: 페이지 크기에 넣어보기

--- one-day-hero/src/app/page.tsx
+++ one-day-hero/src/app/page.tsx
@@ -1,9 +1,13 @@
 import BottomNav from "@/components/common/BottomNav";
+import MissionListItem from "@/components/common/MissionListItem";
 
 const HomePage = () => {
   return (
     <>
       <div>HomePage</div>
+      <div className="w-96 p-5">
+        <MissionListItem />
+      </div>
       <BottomNav />
     </>
   );
