50c1ba785e60d9250643a92b37960e10ae71c33a
jkea1 <jinkyng7890@gmail.com>
2023-10-29 00:39:56 +0900

    feat: 홈 전용 헤더 생성

--- one-day-hero/src/app/page.tsx
+++ one-day-hero/src/app/page.tsx
@@ -1,13 +1,14 @@
 import BottomNav from "@/components/common/BottomNav";
-import MissionListItem from "@/components/common/MissionListItem";
-
 import Button from "@/components/common/Button";
 import DayList from "@/components/common/Day/DayList";
+import HomeHeader from "@/components/common/HomeHeader";
 import Label from "@/components/common/Label";
+import MissionListItem from "@/components/common/MissionListItem";
 
 const HomePage = () => {
   return (
     <>
+      <HomeHeader>동교동</HomeHeader>
       <div>HomePage</div>
       <div className="w-96 p-5">
         <MissionListItem />
