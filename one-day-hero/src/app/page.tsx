f5e89b16ca72fb9efe005ece800c3867dccde172
Howon Shin <shinhw371@gmail.com>
2023-10-29 02:28:59 +0900

    feat: 미션 진행 바 구현

--- one-day-hero/src/app/page.tsx
+++ one-day-hero/src/app/page.tsx
@@ -4,6 +4,7 @@ import MissionListItem from "@/components/common/MissionListItem";
 import Button from "@/components/common/Button";
 import DayList from "@/components/common/Day/DayList";
 import Label from "@/components/common/Label";
+import MissionProgressBar from "@/components/common/MissionProgressBar";
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
       <div>HomePage</div>
       <div className="w-96 p-5">
         <MissionListItem />
+        <MissionProgressBar missionState="matched" />
       </div>
       <DayList />
       <Button />
