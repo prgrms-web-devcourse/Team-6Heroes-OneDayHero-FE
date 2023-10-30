de130dd5350bba7d1d071320691d70dbdbb0ced6
Howon Shin <shinhw371@gmail.com>
2023-10-30 15:04:20 +0900

    39 히어로 지수 (#44)
    * feat: 히어로 지수 large 버전
    
    * feat: small 버전 추가

--- one-day-hero/src/app/page.tsx
+++ one-day-hero/src/app/page.tsx
@@ -1,7 +1,8 @@
 import BottomNav from "@/components/common/BottomNav";
 import Button from "@/components/common/Button";
-import DayList from "@/components/common/Day/DayList";
+import DayList from "@/components/common/DayList";
 import Label from "@/components/common/Label";
+import HeroScore from "@/components/common/HeroScore";
 import MissionInfo from "@/components/common/MissionInfo";
 import MissionListItem from "@/components/common/MissionListItem";
 
@@ -11,6 +12,8 @@ const HomePage = () => {
       <div>HomePage</div>
       <div className="w-96 rounded-[20px] bg-white p-5">
         <MissionListItem />
+        <HeroScore score={20} />
+        <HeroScore size="sm" score={50} />
         <MissionInfo
           missionDay="10/19 목요일"
           missionTime="14:00 ~ 18:00"
