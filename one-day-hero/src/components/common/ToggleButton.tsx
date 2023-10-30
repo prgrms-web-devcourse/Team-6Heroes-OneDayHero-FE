a0c7c700bb8f1b3f2b46672eeb686e2587fb2b41
jkea1 <jinkyng7890@gmail.com>
2023-10-30 23:01:53 +0900

    style: 텍스트 색깔 변경

--- one-day-hero/src/components/common/ToggleButton.tsx
+++ one-day-hero/src/components/common/ToggleButton.tsx
@@ -12,14 +12,14 @@ const ToggleButton = ({
     setClicked(!clicked);
   };
 
-  const defaultStyle = "w-11 h-11 text-base rounded-lg border";
+  const defaultStyle = "w-11 h-11 text-base text-black rounded-lg border";
 
   return (
     <button
       className={`${defaultStyle} ${
         clicked
           ? "border-primary bg-primary-lightest border-4"
-          : "border-base-darken bg-white"
+          : "border-base-darken bg-white "
       } ${className}`}
       onClick={handleButtonClick}>
       {children}
