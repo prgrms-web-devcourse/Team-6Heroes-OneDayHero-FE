a5a3c70c81699e8d59337831dc5df1f1a7a91a09
jkea1 <jinkyng7890@gmail.com>
2023-10-30 15:17:16 +0900

    chore:merge 해결

--- one-day-hero/src/app/page.tsx
+++ one-day-hero/src/app/page.tsx
@@ -1,21 +1,14 @@
 import BottomNav from "@/components/common/BottomNav";
 import Button from "@/components/common/Button";
-<<<<<<< HEAD
-import DayList from "@/components/common/Day/DayList";
-import HomeHeader from "@/components/common/HomeHeader";
-import Label from "@/components/common/Label";
-=======
 import DayList from "@/components/common/DayList";
-import Label from "@/components/common/Label";
 import HeroScore from "@/components/common/HeroScore";
+import Label from "@/components/common/Label";
 import MissionInfo from "@/components/common/MissionInfo";
->>>>>>> develop
 import MissionListItem from "@/components/common/MissionListItem";
 
 const HomePage = () => {
   return (
     <>
-      <HomeHeader>동교동</HomeHeader>
       <div>HomePage</div>
       <div className="w-96 rounded-[20px] bg-white p-5">
         <MissionListItem />
