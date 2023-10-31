70ddcd6c2054f6d82bb4a0eb6ee59875180ac0b9
Howon Shin <shinhw371@gmail.com>
2023-10-31 16:53:01 +0900

    feat: url 합치는 방식 수정

--- one-day-hero/src/services/urls.ts
+++ one-day-hero/src/services/urls.ts
@@ -2,7 +2,7 @@ const makeUrl = (baseUrl: string) => (path: string) => baseUrl + path;
 
 const apiBaseUrl =
   process.env.NEXT_PUBLIC_API_MOCKING === "enabled"
-    ? (process.env.NEXT_PUBLIC_FE_URL ?? "") + "/api/v1/mock"
-    : process.env.NEXT_PUBLIC_BE_URL ?? "";
+    ? `${process.env.NEXT_PUBLIC_FE_URL}/api/v1/mock`
+    : `${process.env.NEXT_PUBLIC_BE_URL}/api/v1`;
 
 export const apiUrl = makeUrl(apiBaseUrl);
