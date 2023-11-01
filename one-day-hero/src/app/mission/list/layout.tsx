404c259b5c0468ce06334c1daa66a152a42d9a3a
jkea1 <jinkyng7890@gmail.com>
2023-11-01 23:48:55 +0900

    feat: 레이아웃 수정

--- one-day-hero/src/app/mission/list/layout.tsx
+++ one-day-hero/src/app/mission/list/layout.tsx
@@ -1,8 +1,18 @@
+import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
+import Tabs from "@/components/common/Tabs";
+
 const MissionListLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <h1>tab</h1>
-      <div>{children}</div>
+      <Header left="back">미션 목록</Header>
+      <div
+        className="fixed
+flex w-full max-w-screen-sm justify-center">
+        <Tabs leftText="진행중인 미션" rightText="제안받은 미션" />
+      </div>
+      {children}
+      <Footer />
     </>
   );
 };
