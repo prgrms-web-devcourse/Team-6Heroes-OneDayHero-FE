49a6e701905fb9228d34b83c6313b617a60671a8
jkea1 <jinkyng7890@gmail.com>
2023-11-02 17:08:29 +0900

    feat: props 속성 추가

--- one-day-hero/src/components/common/MissionProgressContainer.tsx
+++ one-day-hero/src/components/common/MissionProgressContainer.tsx
@@ -2,14 +2,15 @@ import Container from "./Container";
 import MissionProgressBar from "./MissionProgressBar";
 
 const MissionProgressContainer = ({
-  children
+  children,
+  ...props
 }: {
   children: React.ReactNode;
 }) => {
   return (
     <Container className="cs:w-10/12 px-0 pb-0">
       {children}
-      <MissionProgressBar missionState="matched" />
+      <MissionProgressBar missionState="matched" {...props} />
     </Container>
   );
 };
