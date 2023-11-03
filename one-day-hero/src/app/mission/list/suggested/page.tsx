c8a040c9b3eaff5bc93912221b5cb1b6f61feaaa
Jeong-Taeho <nai218218@gmail.com>
2023-11-03 17:26:15 +0900

    style: ListItem 스타일 수정

--- one-day-hero/src/app/mission/list/suggested/page.tsx
+++ one-day-hero/src/app/mission/list/suggested/page.tsx
@@ -4,22 +4,22 @@ import MissionFullInfo from "@/components/common/Info/MissionFullInfo";
 const SuggestedMissionPage = () => {
   return (
     <div className="mt-20 flex w-full max-w-screen-sm flex-col items-center justify-center space-y-4">
-      <Container className="cs:p-4 cs:w-11/12">
+      <Container>
         <MissionFullInfo />
       </Container>
-      <Container className="cs:p-4 cs:w-11/12">
+      <Container>
         <MissionFullInfo />
       </Container>
-      <Container className="cs:p-4 cs:w-11/12">
+      <Container>
         <MissionFullInfo />
       </Container>
-      <Container className="cs:p-4 cs:w-11/12">
+      <Container>
         <MissionFullInfo />
       </Container>
-      <Container className="cs:p-4 cs:w-11/12">
+      <Container>
         <MissionFullInfo />
       </Container>
-      <Container className="cs:p-4 cs:w-11/12">
+      <Container>
         <MissionFullInfo />
       </Container>
     </div>
