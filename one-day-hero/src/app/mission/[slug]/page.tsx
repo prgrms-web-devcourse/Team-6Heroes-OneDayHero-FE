fbca538589f675b6ef55f0e02923bbfe924dc8b7
Howon Shin <shinhw371@gmail.com>
2023-11-03 17:11:58 +0900

    feat: 히어로 추천 전까지 추가

--- one-day-hero/src/app/mission/[slug]/page.tsx
+++ one-day-hero/src/app/mission/[slug]/page.tsx
@@ -4,7 +4,8 @@ import Label from "@/components/common/Label";
 import MissionInfo from "@/components/common/Info/MissionInfo";
 import { getMission } from "@/services/missions";
 import { revalidateTag } from "next/cache";
-import { BiMap } from "react-icons/bi";
+import { BiChevronLeft, BiChevronRight, BiMap } from "react-icons/bi";
+import Button from "@/components/common/Button";
 
 const MissionDetailPage = async ({ params }: { params: { slug: string } }) => {
   revalidateTag(`mission${params.slug}`);
@@ -26,14 +27,29 @@ const MissionDetailPage = async ({ params }: { params: { slug: string } }) => {
         />
       </Container>
       <Container className="cs:w-full">
-        <h2 className="text-md font-semibold">미션 내용</h2>
-        <p>{missionInfo.content}</p>
+        <h2 className="text-base font-semibold">미션 내용</h2>
+        <p className="text-base">{missionInfo.content}</p>
       </Container>
       <Container className="cs:w-full">
-        <IconGroup title={`${region.si} ${region.gu} ${region.dong}`}>
+        <IconGroup
+          title={`${region.si} ${region.gu} ${region.dong}`}
+          direction="row"
+          className="cs:justify-normal cs:mb-2">
           <BiMap />
         </IconGroup>
+        <Button
+          className="cs:w-full cs:h-6 cs:text-black cs:relative hover:bg-inactive-darken"
+          textSize="sm"
+          theme="inactive">
+          미션 지도보기
+          <div className="absolute top-0 right-1 h-6 flex items-center">
+            <BiChevronRight size="20" />
+          </div>
+        </Button>
       </Container>
+      <h1 className="text-lg font-semibold mt-4 break-keep w-full">
+        미션에 딱 맞는 히어로님을 만나보시겠어요?
+      </h1>
     </>
   );
 };
