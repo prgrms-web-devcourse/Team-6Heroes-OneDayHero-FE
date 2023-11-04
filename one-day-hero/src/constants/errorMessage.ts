19bc999ee8c99c4c68237c2bd3e41598965a246d
Jeong-Taeho <nai218218@gmail.com>
2023-11-04 19:15:13 +0900

    feat: 에러 메세지 상수화

---
+++ one-day-hero/src/constants/errorMessage.ts
@@ -0,0 +1,8 @@
+export const FORM_ERROR_MESSAGES = {
+  CHECK_EMPTY_CATEGORY: "카테고리를 선택해주세요.",
+  CHECK_EMPTY_TITLE: "타이틀을 입력하세요.",
+  CHECK_EMPTY_DATE: "미션 날짜를 선택해주세요.",
+  CHECK_SAME_TIME: "미션 타임을 다시 선택해주세요.",
+  CHECK_EMPTY_PRICE: "포상금을 입력해주세요.",
+  CHECK_EMPTY_CONTENT: "미션 내용을 작성해주세요."
+};
