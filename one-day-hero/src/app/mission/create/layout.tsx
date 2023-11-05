841b813837c47baee0acbb8eea18321b8db02377
Jeong-Taeho <nai218218@gmail.com>
2023-11-05 17:08:25 +0900

    fix: footer 버튼 위치 수정

--- one-day-hero/src/app/mission/create/layout.tsx
+++ one-day-hero/src/app/mission/create/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import FooterButton from "@/components/common/FooterButton";
 import Header from "@/components/common/Header";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
@@ -7,6 +8,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
     <>
       <Header>미션 생성</Header>
       {children}
+      <FooterButton formId="missionCreateForm">생성하기</FooterButton>
     </>
   );
 };
