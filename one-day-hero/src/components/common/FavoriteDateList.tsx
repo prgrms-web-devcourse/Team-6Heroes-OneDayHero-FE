4b85b9802cbcaa05e8d3a17c48551bd2e98e622a
Howon Shin <shinhw371@gmail.com>
2023-11-05 23:28:27 +0900

    feat: 히어로 프로필

---
+++ one-day-hero/src/components/common/FavoriteDateList.tsx
@@ -0,0 +1,48 @@
+import { UserResponse } from "@/types/response";
+
+type ArrayElement<ArrayType extends readonly unknown[]> =
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+
+type DateEN = ArrayElement<
+  UserResponse["data"]["favoriteWorkingDay"]["favoriteDate"]
+>;
+
+const DateMap: { [key in DateEN]: string } = {
+  MON: "월",
+  TUE: "화",
+  WED: "수",
+  THU: "목",
+  FRI: "금",
+  SAT: "토",
+  SUN: "일"
+};
+
+type FavoriteDateListProps = {
+  favoriteDate: DateEN[];
+};
+
+const FavoriteDateList = ({ favoriteDate }: FavoriteDateListProps) => {
+  const defaultStyle =
+    "w-11 h-11 text-base text-black rounded-lg border flex justify-center items-center";
+
+  return (
+    <div className="flex flex-wrap gap-x-1.5">
+      {Object.entries(DateMap).map(([dateEN, dateKR]) => {
+        const favored = favoriteDate.includes(dateEN as DateEN);
+
+        return (
+          <div
+            className={`${defaultStyle} ${
+              favored
+                ? "border-4 border-primary bg-primary-lightest"
+                : "border-background-darken bg-white"
+            }`}>
+            {dateKR}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FavoriteDateList;
