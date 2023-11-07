64ec47cbba24e811833238c6ec428d7f5601411b
Jeong-Taeho <nai218218@gmail.com>
2023-11-07 22:36:21 +0900

    fix: Props를 받도록 변경

--- one-day-hero/src/app/notification/page.tsx
+++ one-day-hero/src/app/notification/page.tsx
@@ -5,7 +5,11 @@ import NotificationItem from "@/components/domain/notification/NotificationItem"
 const NotificationPage = () => {
   return (
     <div className="flex w-full flex-col items-center">
-      <NotificationItem />
+      <NotificationItem
+        title="제안받은 미션을 확인해 보세요."
+        content="선량한 시민님이 보낸 '심부름 해주실 분' 미션을 확인해 보세요."
+        createdAt="2023-11-07T00:55:00Z"
+      />
     </div>
   );
 };
