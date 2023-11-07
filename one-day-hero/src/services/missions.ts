8b24b87ffe078d1d21818003f7efdf75ad1e6e4b
Howon Shin <shinhw371@gmail.com>
2023-11-07 15:41:57 +0900

    본인 시점 미션 상세 페이지 구현 (#89)
    * feat: 레이아웃 및 케밥메뉴 초기 설정
    
    * feat: Modal 컴포넌트 구현
    
    * feat: icon size 추가
    
    * feat: 메뉴 박스 구현
    
    * style: 케밥메뉴 용 모달 스타일 정리
    
    * style: 헤더 z-index 추가
    
    * feat: 메뉴 박스 닫는 기능 추가
    
    * chore: convention 틀린 거 수정
    
    * rename: state 업데이트 인수 이름 수정
    
    * chore: convention 오타 수정
    
    * style: 케밥 모달 버튼크기 수정
    
    * chore: mock 데이터 업데이트
    
    * feat: 헤더 케밥 메뉴 추가
    
    * feat: 제목 및 인포 추가
    
    * feat: fetch tag 추가
    
    * chore: merge 후 오류 수정
    
    * feat: 히어로 추천 전까지 추가
    
    * feat: 히어로 추천 리스트 UI 구현
    
    * feat: 수정 버튼 추가

--- one-day-hero/src/services/missions.ts
+++ one-day-hero/src/services/missions.ts
@@ -1,3 +1,4 @@
+import { MissionResponse } from "@/types/response";
 import { apiUrl } from "./urls";
 
 export const getTestMissions = async () => {
@@ -5,6 +6,11 @@ export const getTestMissions = async () => {
   return res.json();
 };
 
-export const getMission = async (missionId: string) => {
-  return fetch(apiUrl(`/missions/${missionId}`)).then((data) => data.json());
+export const getMission = async (
+  missionId: string
+): Promise<MissionResponse> => {
+  const response = await fetch(apiUrl(`/missions/${missionId}`), {
+    next: { tags: [`mission${missionId}`] }
+  });
+  return response.json();
 };
