cefff2eaf978ffc644fa6e614f17bde4568b3569
Jeong-Taeho <nai218218@gmail.com>
2023-11-07 19:23:17 +0900

    feat: 알림 리스트용 컴포넌트 생성

---
+++ one-day-hero/src/components/domain/notification/NotificationBox.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/common/Container";
+import Label from "@/components/common/Label";
+
+const NotificationBox = () => {
+  return (
+    <Container>
+      <Label>서빙</Label>
+      <h1>zzz</h1>
+    </Container>
+  );
+};
+
+export default NotificationBox;
