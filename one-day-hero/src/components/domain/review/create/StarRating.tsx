22e4835059ce6341fa38c7d67ae87417b8c789c7
Jeong-Taeho <nai218218@gmail.com>
2023-11-08 22:44:30 +0900

    feat: 읽기 전용 버전 추가

--- one-day-hero/src/components/domain/review/create/StarRating.tsx
+++ one-day-hero/src/components/domain/review/create/StarRating.tsx
@@ -4,13 +4,23 @@ import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 type StarRatingProps = {
+  value?: 1 | 2 | 3 | 4 | 5;
+  size?: "sm" | "lg";
+  readOnly?: boolean;
   // eslint-disable-next-line no-unused-vars
-  onSelect: (idx: number) => void;
+  onSelect?: (idx: number) => void;
+  className?: string;
 };
 
 const scoreList = [1, 2, 3, 4, 5];
 
-const StarRating = ({ onSelect }: StarRatingProps) => {
+const StarRating = ({
+  value,
+  size = "lg",
+  readOnly,
+  onSelect,
+  className
+}: StarRatingProps) => {
   const [score, setScore] = useState<boolean[]>(
     Array(scoreList.length).fill(false)
   );
@@ -21,17 +31,21 @@ const StarRating = ({ onSelect }: StarRatingProps) => {
     const newScore = resetScore.map((el, idx) => idx <= index && !el);
     setScore(newScore);
 
-    onSelect(index + 1);
+    onSelect && onSelect(index + 1);
   };
 
   return (
-    <div className="flex items-center justify-center gap-2">
+    <div className={`flex items-center justify-center gap-2 ${className}`}>
       {scoreList.map((_, index) => (
         <FaStar
           key={index}
-          size="50"
-          className={`${score[index] ? "text-yellow-400" : "text-inactive"}`}
-          onClick={() => handleClick(index)}
+          size={size === "lg" ? 50 : 12}
+          className={`${
+            (!readOnly ? score[index] : value! > index)
+              ? "text-yellow-400"
+              : "text-inactive"
+          }`}
+          onClick={!readOnly ? () => handleClick(index) : undefined}
         />
       ))}
     </div>
