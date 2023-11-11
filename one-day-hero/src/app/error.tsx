d90703617d761989d2a4a0f5a2edc8b4a49f3d02
Howon Shin <shinhw371@gmail.com>
2023-11-11 23:46:51 +0900

    fix: revalidation 일단 제거

--- one-day-hero/src/app/error.tsx
+++ one-day-hero/src/app/error.tsx
@@ -1,8 +1,9 @@
 "use client"; // Error components must be Client Components
 
-import { revalidatePath } from "next/cache";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+
+import Button from "@/components/common/Button";
 
 export default function ErrorPage({
   error,
@@ -12,7 +13,6 @@ export default function ErrorPage({
   reset?: () => void;
 }) {
   const router = useRouter();
-  const pathname = usePathname();
 
   useEffect(() => {
     console.error(error);
@@ -21,15 +21,14 @@ export default function ErrorPage({
   const handleReset = () => {
     if (reset) reset();
     else {
-      revalidatePath(pathname);
       router.refresh();
     }
   };
 
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-full w-full flex-col items-center justify-center">
       <h2>Something went wrong!</h2>
-      <button onClick={handleReset}>Try again</button>
+      <Button onClick={handleReset}>Try again</Button>
     </div>
   );
 }
