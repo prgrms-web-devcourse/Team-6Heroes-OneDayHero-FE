f073cb25d1f4690cb787b92a2b638a2eb18e2f7a
Howon Shin <shinhw371@gmail.com>
2023-11-11 21:30:51 +0900

    feat: services 함수 변경

--- one-day-hero/src/services/missions.ts
+++ one-day-hero/src/services/missions.ts
@@ -1,87 +1,62 @@
 import { revalidatePath } from "next/cache";
 
 import {
+  BookmarkResponse,
   MissionResponse,
+  OngoingMissionListResponse,
   SuggestedMissionListResponse
 } from "@/types/response";
 
-import { apiUrl } from "./base";
+import { useFetch } from "./base";
 
-export const getTestMissions = async () => {
-  const res = await fetch(apiUrl("/missions"));
-  return res.json();
-};
-
-export const getMission = async (
-  missionId: string
-): Promise<MissionResponse> => {
-  const response = await fetch(apiUrl(`/missions/${missionId}`), {
+export const useGetMissionFetch = (missionId: string) => {
+  return useFetch<MissionResponse>(`/missions/${missionId}`, {
     next: { tags: [`mission${missionId}`] }
   });
-  return response.json();
 };
 
-export const getCompletedMission =
-  async (): Promise<SuggestedMissionListResponse> => {
-    const response = await fetch(apiUrl(`/missions/record`), {
-      next: { tags: ["record"] }
-    });
-
-    return response.json();
-  };
-
-export const postBookmark = async (missionId: number, userId: number) => {
-  const response = await fetch(apiUrl("/bookmarks"), {
-    method: "POST",
-    body: JSON.stringify({
-      missionId,
-      userId
-    })
+export const useGetCompletedMissionFetch = () => {
+  return useFetch<SuggestedMissionListResponse>(`/missions/record`, {
+    next: { tags: ["record"] }
   });
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  revalidatePath("/mission/[slug]", "page");
-
-  return response.json();
 };
 
-export const deleteBookmark = async (missionId: number, userId: number) => {
-  const response = await fetch(apiUrl("/bookmarks"), {
-    method: "DELETE",
-    body: JSON.stringify({
-      missionId,
-      userId
-    })
+export const usePostBookmarkFetch = (missionId: number, userId: number) => {
+  return useFetch<BookmarkResponse>(
+    "/bookmarks",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        missionId,
+        userId
+      })
+    },
+    () => revalidatePath("/mission/[slug]", "page")
+  );
+};
+
+export const useDeleteBookmarkFetch = (missionId: number, userId: number) => {
+  return useFetch<BookmarkResponse>(
+    "/bookmarks",
+    {
+      method: "DELETE",
+      body: JSON.stringify({
+        missionId,
+        userId
+      })
+    },
+    () => revalidatePath("/mission/[slug]", "page")
+  );
+};
+
+export const useGetOngoingMissionListFetch = () => {
+  return useFetch<OngoingMissionListResponse>(`/missions/list/ongoing`, {
+    next: { tags: [`ongoing`] }
   });
-
-  revalidatePath("/mission/[slug]", "page");
-
-  return response.json();
 };
 
-export const getOngoingMissionList = async () => {
-  try {
-    const response = await fetch(apiUrl(`/missions/list/ongoing`), {
-      next: { tags: [`ongoing`] }
-    });
-
-    return response.json();
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-export const getSuggestedMissionList = async () => {
-  try {
-    const response = await fetch(apiUrl(`/missions/list/suggested`), {
-      next: { tags: [`suggested`] }
-    });
-
-    return response.json();
-  } catch (err) {
-    console.error(err);
-  }
+export const useGetSuggestedMissionListFetch = () => {
+  return useFetch<SuggestedMissionListResponse>(`/missions/list/suggested`, {
+    next: { tags: [`suggested`] }
+  });
 };
--- one-day-hero/src/services/users.ts
+++ one-day-hero/src/services/users.ts
@@ -1,10 +1,9 @@
 import { UserResponse } from "@/types/response";
 
-import { apiUrl } from "./base";
+import { useFetch } from "./base";
 
-export const getUser = async (userId: number): Promise<UserResponse> => {
-  const response = await fetch(apiUrl(`/users/${userId}`), {
+export const useGetUserFetch = (userId: number) => {
+  return useFetch<UserResponse>(`/users/${userId}`, {
     next: { tags: [`user${userId}`] }
   });
-  return response.json();
 };
