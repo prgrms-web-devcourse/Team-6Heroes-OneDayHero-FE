77bb611c4027676d86f1cb9ac8c0f7a6ffc05de2
Howon Shin <shinhw371@gmail.com>
2023-11-11 20:38:18 +0900

    feat: error 페이지 추가

---
+++ one-day-hero/src/app/error.tsx
@@ -0,0 +1,35 @@
+"use client"; // Error components must be Client Components
+
+import { revalidatePath } from "next/cache";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset
+}: {
+  error?: Error & { digest?: string };
+  reset?: () => void;
+}) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  const handleReset = () => {
+    if (reset) reset();
+    else {
+      revalidatePath(pathname);
+      router.refresh();
+    }
+  };
+
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <h2>Something went wrong!</h2>
+      <button onClick={handleReset}>Try again</button>
+    </div>
+  );
+}
