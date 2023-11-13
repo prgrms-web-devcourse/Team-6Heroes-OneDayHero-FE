bc2c57c3e993b019d3cb6520fcd875f914041063
Jeong-Taeho <nai218218@gmail.com>
2023-11-13 20:23:08 +0900

    style: margin top 제거 및 패딩 조절

--- one-day-hero/src/components/common/TitleBox.tsx
+++ one-day-hero/src/components/common/TitleBox.tsx
@@ -9,8 +9,10 @@ type TitleBoxProps = {
 const TitleBox = ({ category, title }: TitleBoxProps) => {
   return (
     <Container className="cs:w-full cs:px-5 cs:m-0">
-      <Label size="sm">{category}</Label>
-      <h1 className="mt-1 text-lg font-semibold">{title}</h1>
+      <Label size="sm" className="cs:px-6">
+        {category}
+      </Label>
+      <h1 className="text-lg font-semibold">{title}</h1>
     </Container>
   );
 };
