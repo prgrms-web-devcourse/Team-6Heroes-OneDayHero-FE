786a20e4df81bb73ceafc931b10ead2bd4d27566
jkea1 <jinkyng7890@gmail.com>
2023-11-14 17:34:11 +0900

    feat: 필수조사폼 스키마 및 타입 생성

---
+++ one-day-hero/src/types/schema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const MandatorySurveySchema = z.object({
+  image: z
+    .any()
+    .refine((files) => files?.length == 1, "프로필 이미지를 선택해 주세요."),
+  nickName: z
+    .string()
+    .refine(
+      (name) => name.length > 2 && name.length < 11,
+      "3~10자 길이의 닉네임을 생성해 주세요."
+    ),
+  introduction: z
+    .string()
+    .refine(
+      (intro) => intro.length > 10,
+      "10글자 이상의 자기소개를 작성해 주세요."
+    )
+});
+
+export type MandatorySurveySchemaProps = z.infer<typeof MandatorySurveySchema>;
