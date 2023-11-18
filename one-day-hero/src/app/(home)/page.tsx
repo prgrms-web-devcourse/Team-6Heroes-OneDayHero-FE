b973779fddee8ca9828532304da563a8ac7b3ee6
Jeong-Taeho <nai218218@gmail.com>
2023-11-18 20:36:47 +0900

    feat: 메인 홈 페이지 UI 구현

--- one-day-hero/src/app/(home)/page.tsx
+++ one-day-hero/src/app/(home)/page.tsx
@@ -1,12 +1,38 @@
-import { getServerSession } from "next-auth";
+import Category from "@/components/common/Category";
+import Banner from "@/components/domain/home/Banner";
+import HeroRecommendList from "@/components/domain/missionDetail/HeroRecommendList";
 
-import authOptions from "../api/auth/[...nextauth]/authOptions";
+const defaultLabelStyle = "text-lg font-semibold";
 
 const HomePage = async () => {
-  const session = await getServerSession(authOptions);
   return (
-    <div className="flex w-full flex-col items-center justify-center">
-      {session ? <h1>세션 있음</h1> : <h1>세션 없음</h1>}
+    <div className="flex w-full flex-col gap-8">
+      <div>
+        <Banner />
+      </div>
+      <div className="flex flex-col gap-2">
+        <span className={`${defaultLabelStyle}`}>찾는 카테고리가 있나요?</span>
+        <Category routeState size="lg" className="cs:gap-1" />
+      </div>
+      <div className="h-0 border border-neutral-200" />
+      <div className="flex flex-col gap-2">
+        <span className={`${defaultLabelStyle}`}>이 달의 히어로</span>
+        <HeroRecommendList
+          heroDataList={[
+            { thumbnail: "", nickname: "rabbit", heroScore: 100 },
+            { thumbnail: "", nickname: "rabbit", heroScore: 100 },
+            { thumbnail: "", nickname: "rabbit", heroScore: 100 },
+            { thumbnail: "", nickname: "rabbit", heroScore: 100 },
+            { thumbnail: "", nickname: "rabbit", heroScore: 100 },
+            { thumbnail: "", nickname: "rabbit", heroScore: 100 },
+            { thumbnail: "", nickname: "rabbit", heroScore: 100 }
+          ]}
+        />
+      </div>
+      <div className="h-0 border border-neutral-200" />
+      <div>
+        <span className={`${defaultLabelStyle}`}>곧 마감돼요!</span>
+      </div>
     </div>
   );
 };
