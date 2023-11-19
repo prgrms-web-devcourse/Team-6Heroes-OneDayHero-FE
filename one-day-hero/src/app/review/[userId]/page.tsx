e9a6d128b802c03970997479060e66b402c94ec4
Jeong-Taeho <nai218218@gmail.com>
2023-11-19 21:03:37 +0900

    feat: 내가 쓴 리뷰 페이지 구현

--- one-day-hero/src/app/review/[userId]/page.tsx
+++ one-day-hero/src/app/review/[userId]/page.tsx
@@ -1,5 +1,43 @@
-const page = () => {
-  return <div>목록 페이지 들어갈 부분</div>;
+import { revalidateTag } from "next/cache";
+import Link from "next/link";
+
+import ReviewInfo from "@/components/domain/review/ReviewInfo";
+import { useGetSendReviewFetch } from "@/services/review";
+
+const SendReviewPage = async () => {
+  revalidateTag("sendReview");
+
+  const { data, hasNextPage, fetchNextPage } = await useGetSendReviewFetch();
+
+  return (
+    <div className="w-full">
+      {data &&
+        data.map(
+          ({
+            reviewId,
+            categoryName,
+            missionTitle,
+            starScore,
+            createdAt,
+            senderNickName
+          }) => (
+            <Link
+              key={reviewId}
+              href={`/review/1/${reviewId}`}
+              className="flex w-full justify-center">
+              <ReviewInfo
+                categoryName={categoryName}
+                content={missionTitle}
+                starScore={starScore}
+                createdAt={createdAt}
+                senderNickName={senderNickName}
+                reviewId={reviewId}
+              />
+            </Link>
+          )
+        )}
+    </div>
+  );
 };
 
-export default page;
+export default SendReviewPage;
