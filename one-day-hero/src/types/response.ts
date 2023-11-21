f20ea59ac97bad5ac90d48a41b18839cadd821a1
jkea1 <jinkyng7890@gmail.com>
2023-11-21 23:29:15 +0900

    feat: 미션 검색 리스트 응답 타입 생성

--- one-day-hero/src/types/response.ts
+++ one-day-hero/src/types/response.ts
@@ -236,3 +236,37 @@ export type ReviewDetailResponse = {
   };
   serverDateTime: string;
 };
+
+export type MissionSearchListResponse = {
+  status: number;
+  data: {
+    content: {
+      id: number;
+      mission: MissionItemResponse;
+    }[];
+    pageable: {
+      pageNumber: number;
+      pageSize: number;
+      sort: {
+        sorted: boolean;
+        unsorted: boolean;
+        empty: boolean;
+      };
+      offset: number;
+      paged: boolean;
+      unpaged: boolean;
+    };
+    numberOfElements: number;
+    first: boolean;
+    last: boolean;
+    size: number;
+    number: number;
+    sort: {
+      sorted: boolean;
+      unsorted: boolean;
+      empty: boolean;
+    };
+    empty: boolean;
+  };
+  serverDateTime: string;
+};
