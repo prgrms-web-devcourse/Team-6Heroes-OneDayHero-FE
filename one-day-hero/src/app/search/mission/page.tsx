cd86fb8361dac52400d52008c5c4f460a6d644cd
jkea1 <jinkyng7890@gmail.com>
2023-11-21 23:43:22 +0900

    chore: 줄 간격 조정

--- one-day-hero/src/app/search/mission/page.tsx
+++ one-day-hero/src/app/search/mission/page.tsx
@@ -28,6 +28,7 @@ const MissionSearchPage = () => {
   const [fetchedData, setFetchedData] = useState<fetchDataProps>();
 
   const selectedCategoryId = Number(useSearchParams().get("category"));
+
   const [categoryId, setCategoryId] = useState<
     0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | number
   >(selectedCategoryId ?? 0);
