af642d1778f1deee3541bf9e34484bdfd171fc42
Howon Shin <shinhw371@gmail.com>
2023-11-21 15:02:06 +0900

    fix: 콘솔 제거

--- one-day-hero/src/app/mission/create/page.tsx
+++ one-day-hero/src/app/mission/create/page.tsx
@@ -1,8 +1,6 @@
-import { getServerToken } from "@/app/utils/auth";
 import CreateForm from "@/components/domain/mission/create/CreateForm";
 
 const MissionCreatePage = () => {
-  console.log(getServerToken());
   return (
     <div className="flex w-full flex-col items-center">
       <CreateForm />
