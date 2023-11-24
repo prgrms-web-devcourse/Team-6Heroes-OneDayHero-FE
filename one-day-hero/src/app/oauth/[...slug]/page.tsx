c67db40c693e62474ccf22011c887bef164e051e
Howon Shin <shinhw371@gmail.com>
2023-11-24 16:50:01 +0900

    feat: userId context 생성 (#188)

--- one-day-hero/src/app/oauth/[...slug]/page.tsx
+++ one-day-hero/src/app/oauth/[...slug]/page.tsx
@@ -3,24 +3,52 @@
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
+import { useToast } from "@/contexts/ToastProvider";
+import { useUserId } from "@/contexts/UserIdProvider";
+
 const KakaoCallbackPage = () => {
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
 
   const router = useRouter();
 
+  const { setUserId } = useUserId();
+
+  const { showToast } = useToast();
+
   useEffect(() => {
     if (!code) return;
 
     const postCode = async () => {
-      await fetch(`${process.env.NEXT_PUBLIC_FE_URL}/api/token`, {
-        method: "POST",
-        body: JSON.stringify({
-          code
-        })
-      });
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_FE_URL}/api/token`,
+          {
+            method: "POST",
+            body: JSON.stringify({
+              code
+            })
+          }
+        );
 
-      router.push("/");
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+
+        const userId = (await response.json()).userId;
+        setUserId(userId);
+
+        showToast("로그인 성공!", "success");
+        router.push("/");
+      } catch (err) {
+        console.error(err);
+
+        showToast(
+          "로그인 처리 중 에러가 발생했습니다. 다시 시도해주세요.",
+          "error"
+        );
+        router.push("/login");
+      }
     };
 
     postCode();
