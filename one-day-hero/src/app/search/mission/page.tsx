823fba5fde05a60fdf0e10ce61499238694d36aa
jkea1 <jinkyng7890@gmail.com>
2023-11-26 01:29:39 +0900

    feat: 미션 검색 페이지 구현

--- one-day-hero/src/app/search/mission/page.tsx
+++ one-day-hero/src/app/search/mission/page.tsx
@@ -1,87 +1,121 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { ChangeEvent, useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useRouter, useSearchParams } from "next/navigation";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-import { favoriteRegionsData } from "@/app/api/v1/mock/_data/survey";
+import { getClientToken } from "@/app/utils/cookie";
 import Category from "@/components/common/Category";
 import Container from "@/components/common/Container";
 import MissionFullInfo from "@/components/common/Info/MissionFullInfo";
 import Select from "@/components/common/Select";
-import { useGetSuggestedMissionListFetch } from "@/services/missions";
-import { MissionItemResponse } from "@/types/response";
-import {
-  MissionSearchFilterSchema,
-  MissionSearchFilterSchemaProps
-} from "@/types/schema";
+import { useGetRegionsFetch } from "@/services/regions";
+import { useGetMissionSearchListFetch } from "@/services/search";
 
-/**@note 이건 나중에 변경되거나 빠질 거 같아요. */
-type fetchDataProps = { id: number; mission: MissionItemResponse }[];
+type dongProps = { id: number; dong: string };
+
+type guProps = { gu: string; dong: dongProps[] }[];
 
 const MissionSearchPage = () => {
-  const [gu, setGu] = useState<string>("");
-  const [dong, setDong] = useState<string>("");
-  const [dongId, setDongId] = useState<number>(0);
-  const [fetchedData, setFetchedData] = useState<fetchDataProps>();
+  const [guData, setGuData] = useState<guProps>([]);
+  const [selectedGu, setSelectedGu] = useState<string>();
+  const [dongData, setDongData] = useState<dongProps[]>();
+  const [selectedDong, setSelectedDong] = useState<string>();
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number>();
+  const [dongId, setDongId] = useState<string>();
+  const [sort, setSort] = useState<string>("");
 
-  const selectedCategoryId = Number(useSearchParams().get("category"));
+  const guRef = useRef<HTMLSelectElement | null>(null);
+  const dongRef = useRef<HTMLSelectElement | null>(null);
 
-  const [categoryId, setCategoryId] = useState<
-    0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | number
-  >(selectedCategoryId ?? 0);
+  const token = getClientToken();
+  const observerRef = useRef<HTMLDivElement | null>(null);
 
-  const { setValue, getValues } = useForm<MissionSearchFilterSchemaProps>({
-    resolver: zodResolver(MissionSearchFilterSchema)
-  });
+  const { data, setSearchParams } = useGetMissionSearchListFetch(
+    token ?? "",
+    observerRef
+  );
+
+  const { mutationalFetch } = useGetRegionsFetch(token ?? "");
+
+  const queryString = Number(useSearchParams().get("category"));
+  const router = useRouter();
 
   useEffect(() => {
-    const categoryId = String(getValues("categoryId"));
-    const regionId = String(getValues("dongId"));
+    handleCategorySelect(queryString);
+    router.push("/search/mission");
+  }, []);
 
-    const fetchData = async () => {
+  useEffect(() => {
+    const fetchRegions = async () => {
       try {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const { data } = await useGetSuggestedMissionListFetch("1");
-        console.log("for check", categoryId, regionId);
-        setFetchedData(data);
-      } catch (error) {
-        console.error(
-          "미션 검색 데이터를 불러오는 중 오류가 발생했어요:",
-          error
-        );
+        const { response, isError, errorMessage } = await mutationalFetch({
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+
+        if (response) {
+          const { gu } = response.data[0];
+
+          setGuData(gu);
+        } else if (isError) {
+          console.log(errorMessage);
+        }
+      } catch (err) {
+        console.error(err);
       }
     };
 
-    fetchData();
-  }, [dongId, getValues, categoryId]);
+    fetchRegions();
+  }, []);
 
-  const seoulGu = favoriteRegionsData["서울시"].map(
-    (region) => Object.keys(region)[0]
-  );
+  useEffect(() => {
+    const dong = guData?.find((item) => item.gu === selectedGu)?.dong;
 
-  const seoulDong = favoriteRegionsData["서울시"].filter(
-    (region) => gu in region
-  )?.[0]?.[gu];
+    setDongData(dong);
+  }, [selectedGu]);
 
-  const handleGuChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setGu(e.target.value);
+  const makeSortValue = (dongId?: string, selectedCategoryId?: number) => {
+    if (selectedCategoryId && dongId) {
+      setSort(
+        `&missionCategoryCodes=MC_00${selectedCategoryId}&regionIds=${dongId}`
+      );
+    } else if (selectedCategoryId && !dongId) {
+      setSort(`&missionCategoryCodes=MC_00${selectedCategoryId}`);
+    } else if (!selectedCategoryId && dongId) {
+      setSort(`&regionIds=${dongId}`);
+    }
   };
 
-  const handleDongChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setDong(e.target.options[e.target.selectedIndex].dataset.dong ?? "");
-    setDongId(Number(e.target.value));
+  useEffect(() => {
+    setSearchParams(sort);
+  }, [sort]);
 
-    setValue("dongId", String(dongId));
+  const handleGuSelect = () => {
+    const gu = guRef.current?.value;
+
+    setSelectedGu(gu);
+  };
+
+  const handleDongSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    const dong = dongRef.current?.value;
+    const dongId = e.target.options[e.target.selectedIndex].dataset.id;
+
+    setSelectedDong(dong);
+    setDongId(dongId);
+
+    makeSortValue(dongId, selectedCategoryId);
   };
 
   const handleCategorySelect = (id: number) => {
-    setCategoryId(id);
-
-    setValue("categoryId", String(categoryId));
+    setSelectedCategoryId(id);
+    makeSortValue(dongId, id);
   };
+
+  const SelectDefaultStyle =
+    "border-inactive focus:outline-primary w-full h-[2.13rem] rounded-[0.625rem] border pl-2";
 
   return (
     <>
@@ -89,63 +123,77 @@ const MissionSearchPage = () => {
         className="fixed z-50 mt-[4.5rem] 
 w-full max-w-screen-sm">
         <div className="mx-4 mt-1 flex gap-2">
-          <Select id="si">
-            <option value="seoul" selected hidden>
-              서울시
+          <Select value="서울특별시">
+            <option value="서울특별시" key={uuidv4()}>
+              서울특별시
             </option>
           </Select>
 
-          <Select id="gu" onChange={handleGuChange}>
-            {seoulGu.map((gu) => (
-              <option className="text-xs" key={gu} value={gu}>
-                {gu}
-              </option>
-            ))}
-          </Select>
+          <select
+            id="gu"
+            ref={guRef}
+            onChange={handleGuSelect}
+            className={SelectDefaultStyle}>
+            <option>{selectedGu ?? "선택"}</option>
+            {guData &&
+              guData.map((data) => (
+                <option className="text-xs" key={uuidv4()}>
+                  {data.gu}
+                </option>
+              ))}
+          </select>
 
-          <Select id="dong" onChange={handleDongChange}>
-            {seoulDong &&
-              seoulDong.map(
-                ({ regionId, dong }: { regionId: number; dong: string }) => (
-                  <option
-                    className="text-xs"
-                    key={regionId}
-                    value={regionId}
-                    data-dong={dong}>
-                    {dong}
-                  </option>
-                )
-              )}
-          </Select>
+          <select
+            id="dong"
+            ref={dongRef}
+            onChange={handleDongSelect}
+            className={SelectDefaultStyle}>
+            <option>{selectedDong ?? "선택"}</option>
+            {dongData &&
+              dongData.map(({ id, dong }: dongProps) => (
+                <option
+                  className="text-xs"
+                  key={id + uuidv4()}
+                  value={dong}
+                  data-id={id}>
+                  {dong}
+                </option>
+              ))}
+          </select>
         </div>
 
         <div className="border-background-darken mt-3 flex justify-center border-b pb-2">
-          <Category
-            onSelect={handleCategorySelect}
-            value={categoryId}
-            size="sm"
-          />
+          <Category onSelect={handleCategorySelect} size="sm" />
         </div>
       </section>
 
       <section className="mt-56 flex w-full max-w-screen-sm flex-col items-center justify-center space-y-4">
-        {fetchedData &&
-          fetchedData.map(({ id, mission }) => (
+        {data.map(
+          ({
+            id,
+            bookmarkCount,
+            missionInfo,
+            region,
+            missionStatus,
+            missionCategory
+          }) => (
             <Link
               href={`/mission/${id}`}
               className="flex w-full max-w-screen-sm justify-center"
               key={id}>
-              <Container className="cs:w-11/12" missionStatus={mission.status}>
+              <Container className="cs:w-11/12" missionStatus={missionStatus}>
                 <MissionFullInfo
-                  bookmarkCount={mission.bookmarkCount}
-                  createdAt={mission.createdAt}
-                  region={mission.region}
-                  missionCategory={mission.missionCategory}
-                  missionInfo={mission.missionInfo}
+                  bookmarkCount={bookmarkCount}
+                  createdAt={missionInfo.missionDate}
+                  region={region}
+                  missionCategory={missionCategory}
+                  missionInfo={missionInfo}
                 />
               </Container>
             </Link>
-          ))}
+          )
+        )}
+        <div ref={observerRef} />
       </section>
     </>
   );
