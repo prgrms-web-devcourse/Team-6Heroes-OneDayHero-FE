92ff5ef2568c43af6e8b3482198660779268ed69
jkea1 <jinkyng7890@gmail.com>
2023-11-27 04:40:48 +0900

    feat: 히어로 검색 페이지 생성

--- one-day-hero/src/app/search/hero/page.tsx
+++ one-day-hero/src/app/search/hero/page.tsx
@@ -1,16 +1,66 @@
-import { FaSearch } from "react-icons/fa";
+"use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
+
+import { getClientToken } from "@/app/utils/cookie";
 import Button from "@/components/common/Button";
-import Input from "@/components/common/Input";
+import Container from "@/components/common/Container";
+import HeroScore from "@/components/common/HeroScore";
+import { useGetHeroNicknameDetailListFetch } from "@/services/search";
+import DefaultThumbnail from "/public/images/OneDayHero_logo_sm.svg";
 
 const HeroSearchPage = () => {
+  const [inputValue, setInputValue] = useState<string | null>(null);
+
+  const token = getClientToken();
+  const observerRef = useRef<HTMLDivElement | null>(null);
+
+  const { data, setSearchParams } = useGetHeroNicknameDetailListFetch(
+    token ?? "",
+    observerRef
+  );
+
+  const searchHandler = useCallback(() => {
+    if (inputValue === "") {
+      setSearchParams("");
+      return;
+    }
+
+    const encodingValue = encodeURI(inputValue!);
+
+    setSearchParams(`nickname=${encodingValue}`);
+  }, [inputValue]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      searchHandler();
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchHandler]);
+
+  const InputDefaultStyle =
+    "rounded-[0.65rem] h-11 w-full border border-inactive focus:outline-primary placeholder:text-inactive pl-3";
+
   return (
     <>
       <div
         className="fixed z-50 mt-[4.5rem] 
 w-full max-w-screen-sm">
         <section className="border-background-darken flex justify-center border-b px-4 pb-6">
-          <Input className="cs:h-11" />
+          <input
+            className={InputDefaultStyle}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+            placeholder="히어로를 검색해 보세요."
+          />
           <Button
             theme="primary"
             className="cs:h-11 cs:w-11 cs:rounded-xl cs:px-3 cs:ml-2 cs:border-inactive border-2">
@@ -18,13 +68,33 @@ w-full max-w-screen-sm">
           </Button>
         </section>
       </div>
-      <section className="mt-44 flex w-full max-w-screen-sm flex-col items-center justify-center space-y-4">
-        <div className="h-52 w-72 bg-green-400">임시</div>
-        <div className="h-52 w-72 bg-green-400">임시</div>
-        <div className="h-52 w-72 bg-green-400">임시</div>
-        <div className="h-52 w-72 bg-green-400">임시</div>
-        <div className="h-52 w-72 bg-green-400">임시</div>
-        <div className="h-52 w-72 bg-green-400">임시</div>
+
+      <section className="mt-40 flex w-full max-w-screen-sm flex-col items-center justify-center gap-y-4">
+        {data.length !== 0 ? (
+          data.map(({ nickname, id, heroScore, image }) => (
+            <Link href="/search/hero" key={uuidv4()} className="w-full">
+              <Container className="cs:mx-auto">
+                <div className="mt-2 flex pr-2">
+                  <Image
+                    src={image.path ?? DefaultThumbnail}
+                    alt="프로필 이미지"
+                    width={60}
+                    className="pointer-events-none mr-2 rounded-full"
+                  />
+                  <div className="grow">
+                    <h3 className="text-base font-semibold">{nickname}</h3>
+                    <HeroScore score={heroScore} size="sm" />
+                  </div>
+                </div>
+              </Container>
+            </Link>
+          ))
+        ) : (
+          <div className="text-cancel-lighten">
+            해당 히어로가 존재하지 않습니다.
+          </div>
+        )}
+        <div ref={observerRef} />
       </section>
     </>
   );
