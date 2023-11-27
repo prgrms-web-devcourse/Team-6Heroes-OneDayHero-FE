a7525415234544c261a7a2afe9d5c0f867267f68
Jeong-Taeho <nabi218218@gmail.com>
2023-11-28 01:04:41 +0900

    feat: 클라이언트 단에서 데이터 패치

--- one-day-hero/src/components/domain/notification/NotificationItem.tsx
+++ one-day-hero/src/components/domain/notification/NotificationItem.tsx
@@ -6,10 +6,15 @@ import { BsFillCircleFill } from "react-icons/bs";
 import { getClientToken } from "@/app/utils/cookie";
 import { formatDate } from "@/app/utils/formatDate";
 import Container from "@/components/common/Container";
+import { useNotification } from "@/contexts/NotificationProvider";
 import { useGetNotificationFetch } from "@/services/notification";
 
 const NotificationItem = () => {
   const observerRef = useRef<HTMLDivElement | null>(null);
+
+  const { setAlarmStatus } = useNotification();
+
+  setAlarmStatus(false);
 
   const token = getClientToken();
 
