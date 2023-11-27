80eb5f1e2626a7bd17b42ec75c3ad4adb87e8808
Jeong-Taeho <nabi218218@gmail.com>
2023-11-27 23:39:57 +0900

    feat: 알림 조회 무한 스크롤 api로 변경

--- one-day-hero/src/components/domain/notification/NotificationItem.tsx
+++ one-day-hero/src/components/domain/notification/NotificationItem.tsx
@@ -1,26 +1,40 @@
+"use client";
+
+import { useRef } from "react";
 import { BsFillCircleFill } from "react-icons/bs";
 
+import { getClientToken } from "@/app/utils/cookie";
 import { formatDate } from "@/app/utils/formatDate";
 import Container from "@/components/common/Container";
+import { useGetNotificationFetch } from "@/services/notification";
 
-type NotificationProps = {
-  title: string;
-  content: string;
-  createdAt: string;
-};
+const NotificationItem = () => {
+  const observerRef = useRef<HTMLDivElement | null>(null);
 
-const NotificationItem = ({ title, content, createdAt }: NotificationProps) => {
-  const date = formatDate(createdAt);
+  const token = getClientToken();
+
+  const { data } = useGetNotificationFetch(token ?? "", observerRef);
+
+  console.log(data);
 
   return (
-    <Container className="cs:w-full cs:p-4 flex">
-      <div className="flex w-full flex-col gap-2">
-        <h1 className="text-base font-semibold">{title}</h1>
-        <p className="text-sm">{content}</p>
-        <p className="text-inactive text-xs">{date}</p>
-      </div>
-      <BsFillCircleFill size="11" className="text-inactive" />
-    </Container>
+    <>
+      {data.length
+        ? data.map((list) => (
+            <Container key={list.id} className="cs:w-full cs:p-4 flex">
+              <div className="flex w-full flex-col gap-2">
+                <h1 className="text-base font-semibold">{list.title}</h1>
+                <p className="text-sm">{list.content}</p>
+                <p className="text-inactive text-xs">
+                  {formatDate(list.createdAt)}
+                </p>
+              </div>
+              <BsFillCircleFill size="11" className="text-inactive" />
+            </Container>
+          ))
+        : null}
+      <div ref={observerRef} />
+    </>
   );
 };
 
