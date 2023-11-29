603425274e47939404ac6ecd26796772b541817f
Howon Shin <shinhw371@gmail.com>
2023-11-29 16:17:24 +0900

    fix: 메인에 있던 파일 삭제

--- one-day-hero/src/components/domain/mission/create/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-
-const ErrorMessage = ({ children }: PropsWithChildren) => {
-  const defaultStyle = "text-red-500 text-sm mt-1";
-
-  return <span className={`${defaultStyle}`}>{children}</span>;
-};
-
-export default ErrorMessage;
