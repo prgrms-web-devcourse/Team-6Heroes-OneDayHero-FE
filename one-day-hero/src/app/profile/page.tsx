2a361ce9556f0b7d962c92a9469bf0ea13605e94
jkea1 <jinkyng7890@gmail.com>
2023-12-01 16:20:25 +0900

    feat: 선호시간이 없는 경우 ~ 안 보이게 하는 처리

--- one-day-hero/src/app/profile/page.tsx
+++ one-day-hero/src/app/profile/page.tsx
@@ -87,15 +87,18 @@ const ProfilePage = async () => {
       </div>
       <div className="w-full">
         <h2 className="mb-2 mt-5 text-xl font-semibold">희망 근무시간</h2>
-        <div className="min-h-[2.625rem] rounded-lg border border-background-darken bg-white p-2">
-          {`${favoriteWorkingDay.favoriteStartTime || ""} ~ ${
-            favoriteWorkingDay.favoriteEndTime || ""
-          }`}
+        <div className="border-background-darken min-h-[2.625rem] rounded-lg border bg-white p-2">
+          {`${favoriteWorkingDay.favoriteStartTime || ""} ${
+            favoriteWorkingDay.favoriteStartTime &&
+            favoriteWorkingDay.favoriteEndTime
+              ? "~"
+              : ""
+          } ${favoriteWorkingDay.favoriteEndTime || ""}`}
         </div>
       </div>
       <div className="w-full">
         <h2 className="mb-2 mt-5 text-xl font-semibold">선호 지역</h2>
-        <div className="min-h-[2.625rem] rounded-lg border border-background-darken bg-white p-2">
+        <div className="border-background-darken min-h-[2.625rem] rounded-lg border bg-white p-2">
           {favoriteRegions?.map(({ id, si, gu, dong }) => (
             <p key={id}>{`${si} ${gu} ${dong}`}</p>
           ))}
@@ -103,7 +106,7 @@ const ProfilePage = async () => {
       </div>
       <div className="mb-12 w-full">
         <h2 className="mb-2 mt-5 text-xl font-semibold">소개</h2>
-        <div className="rounded-lg border border-background-darken bg-white p-2">
+        <div className="border-background-darken rounded-lg border bg-white p-2">
           <p>{basicInfo.introduce}</p>
         </div>
       </div>
