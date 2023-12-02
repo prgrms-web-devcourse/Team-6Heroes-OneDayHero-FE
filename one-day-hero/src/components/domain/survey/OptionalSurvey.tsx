acfaa8006167b86199340dd6fcd25b6a01a2f11d
Howon Shin <shinhw371@gmail.com>
2023-12-03 06:37:50 +0900

    fix: 선호 시간 빈 값 처리 수정

--- one-day-hero/src/components/domain/survey/OptionalSurvey.tsx
+++ one-day-hero/src/components/domain/survey/OptionalSurvey.tsx
@@ -171,12 +171,12 @@ const OptionalSurvey = (userData: ProfileResponse) => {
 
     const { favoriteWorkingDay, favoriteRegions } = data;
 
-    if (favoriteWorkingDay?.favoriteStartTime === "선택") {
-      favoriteWorkingDay?.favoriteStartTime === null;
+    if (favoriteWorkingDay && favoriteWorkingDay.favoriteStartTime === "선택") {
+      favoriteWorkingDay.favoriteStartTime = undefined;
     }
 
-    if (favoriteWorkingDay?.favoriteEndTime === "선택") {
-      favoriteWorkingDay?.favoriteEndTime === null;
+    if (favoriteWorkingDay && favoriteWorkingDay.favoriteEndTime === "선택") {
+      favoriteWorkingDay.favoriteEndTime = undefined;
     }
 
     const userData: UserInfoForOptionalSurveyResponse = {
