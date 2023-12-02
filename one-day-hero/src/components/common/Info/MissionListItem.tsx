cead023fad25f46c9b02d10b7fd5a9d0f509de1b
Howon Shin <shinhw371@gmail.com>
2023-12-02 16:28:48 +0900

    채팅창 엔터 isComposing 처리 추가 (#269)
    * fix: isComposing 처리 추가
    
    * fix: 어색한 스타일 수정

--- one-day-hero/src/components/common/Info/MissionListItem.tsx
+++ one-day-hero/src/components/common/Info/MissionListItem.tsx
@@ -28,7 +28,7 @@ const MissionListItem = ({
   return (
     <div className={`flex w-full ${className}`}>
       <div className="flex grow gap-4">
-        <div className="bg-inactive overflow-hidden rounded-[0.625rem]">
+        <div className="h-[3.75rem] shrink-0 overflow-hidden rounded-[0.625rem] bg-inactive">
           <Image
             src={imageSrc || defaultProfileImage}
             alt="프로필 사진"
