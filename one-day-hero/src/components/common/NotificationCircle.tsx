760b9758696d377b5836a95eb9bfbc5a8407f7de
Howon Shin <shinhw371@gmail.com>
2023-12-03 04:55:21 +0900

    fix: 홈페이지 하이드레이션 오류 수정

--- one-day-hero/src/components/common/NotificationCircle.tsx
+++ one-day-hero/src/components/common/NotificationCircle.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import { getLocalStorage } from "@/utils/storage";
 
 const NotificationCircle = () => {
-  const alarmStatus = getLocalStorage("sse");
+  const [alarmStatus, setAlarmStatus] = useState<boolean>(false);
+
+  useEffect(() => {
+    setAlarmStatus(getLocalStorage("sse", false));
+  }, []);
 
   return (
     <>
       {alarmStatus ? (
-        <div className="bg-active absolute right-0 top-0 h-2.5 w-2.5 animate-ping rounded-full" />
+        <div className="animate-ping absolute right-0 top-0 h-2.5 w-2.5 rounded-full bg-active" />
       ) : null}
     </>
   );
--- one-day-hero/src/components/domain/home/HomeMissionList.tsx
+++ one-day-hero/src/components/domain/home/HomeMissionList.tsx
@@ -13,8 +13,8 @@ import { getClientToken } from "@/utils/cookie";
 
 const HomeMissionList = () => {
   const [soonMissionList, setSoonMissionList] = useState<
-    HomeResponse["data"]["soonExpiredMissions"] | null
-  >(null);
+    HomeResponse["data"]["soonExpiredMissions"]
+  >([]);
   const { location } = useLocation();
   const token = getClientToken();
 
@@ -38,32 +38,29 @@ const HomeMissionList = () => {
 
   return (
     <div className="mt-7 flex flex-col gap-3">
-      {soonMissionList?.length ? (
-        soonMissionList?.map((mission) => (
-          <Link
-            key={mission.id}
-            href={`/mission/${mission.id}`}
-            className="flex w-full justify-center">
-            <Container className="cs:w-full">
-              <MissionFullInfo
-                missionImagePath={mission.imagePath}
-                missionCategory={mission.missionCategory}
-                region={mission.region}
-                bookmarkCount={mission.bookmarkCount}
-                missionInfo={{
-                  title: mission.title,
-                  missionDate: mission.missionDate,
-                  startTime: mission.startTime,
-                  endTime: mission.endTime,
-                  price: mission.price
-                }}
-              />
-            </Container>
-          </Link>
-        ))
-      ) : (
-        <h1>아직 생성된 미션이 없어요..</h1>
-      )}
+      {soonMissionList.map((mission) => (
+        <Link
+          key={mission.id}
+          href={`/mission/${mission.id}`}
+          className="flex w-full justify-center">
+          <Container className="cs:w-full">
+            <MissionFullInfo
+              missionImagePath={mission.imagePath}
+              missionCategory={mission.missionCategory}
+              region={mission.region}
+              bookmarkCount={mission.bookmarkCount}
+              missionInfo={{
+                title: mission.title,
+                missionDate: mission.missionDate,
+                startTime: mission.startTime,
+                endTime: mission.endTime,
+                price: mission.price
+              }}
+            />
+          </Container>
+        </Link>
+      ))}{" "}
+      {soonMissionList.length <= 0 && <h1>아직 생성된 미션이 없어요..</h1>}
     </div>
   );
 };
